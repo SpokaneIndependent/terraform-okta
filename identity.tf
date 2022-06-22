c4d48d7fa6a3bb7ef584c11c838cdbdd9d286d79
Phillip Hocking <phillhocking@gmail.com>
2022-06-21 19:25:58 -0700

    Added okta_org_configuration in identity.tf fixed typo for domain

--- identity.tf
+++ identity.tf
@@ -1,5 +1,10 @@
+resource "okta_org_configuration" "dev" {
+  company_name = "Spokane Independant Metropolitan Business Alliance"
+  website      = "https://spokaneindependent.org"
+}
+
 resource "okta_domain" "dev" {
-  name   = "spokaneindependent.com"
+  name   = "spokaneindependent.org"
 }
 
 resource "okta_domain_verification" "dev" {
