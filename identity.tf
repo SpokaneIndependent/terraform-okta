d1877e7c49a2c83dd84e415d72c7349bec7b8811
Phillip Hocking <phillhocking@gmail.com>
2022-06-30 20:23:42 -0700

    Added organizational details in identity.tf

--- identity.tf
+++ identity.tf
@@ -1,6 +1,13 @@
 resource "okta_org_configuration" "dev" {
-  company_name = "Spokane Independant Metropolitan Business Alliance"
-  website      = "https://spokaneindependent.org"
+  company_name         = "Spokane Independant Metropolitan Business Alliance"
+  website              = "https://spokaneindependent.org"
+  phone_number         = "(509) 939-0015"
+  support_phone_number = "(818) 457-6605"
+  address_1            = "PO Box 40344"
+  city                 = "Spokane"
+  state                = "Washington"
+  country              = "United States"
+  postal_code          = "99202"
 }
 
 resource "okta_domain" "dev" {
